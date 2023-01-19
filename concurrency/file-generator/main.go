29da176d40fb1ec42519f361bcc50a7c0e4ff491
Jaume Jiménez <jaume.jimenezforteza@metro-markets.de>
2023-01-19 15:41:34 +0100

    Init project with file-generator subproject

---
+++ concurrency/file-generator/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "file:./file-generator-db")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	var version string
+	err = db.QueryRow("SELECT SQLITE_VERSION()").Scan(&version)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(version)
+}
