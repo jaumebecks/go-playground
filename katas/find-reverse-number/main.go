276cd431e42467e7a539d92a416e3ce90bb38ffd
Jaume Jiménez <jaume.jimenezforteza@metro-markets.de>
2025-11-06 14:32:10 +0100

    FindReverseNumber: Added timeout execution helper

--- katas/find-reverse-number/main.go
+++ katas/find-reverse-number/main.go
@@ -7,21 +7,46 @@ import (
 	"time"
 )
 
+const n uint64 = 100000
+const timeout = 12 * time.Second
+
 func main() {
-	var start time.Time
-	// start = time.Now()
-	// fmt.Println(FindReverseNumberV1(100000)) // 2min46s
-	// fmt.Println("V1 took ", time.Since(start))
+	// FindReverseNumberV1(n=100000) // 2min46s
+	// FindReverseNumberV2(n=100000) // 8,9s
+	// FindReverseNumberV3(n=100000) // 10.25s -> slower than V2
 
-	start = time.Now()
-	fmt.Println(FindReverseNumberV2(100000)) // 8,9s
-	fmt.Printf("V2 took %s\n", time.Since(start))
+	r1, err1 := execWithTimeout(n, FindReverseNumberV1)
+	r2, err2 := execWithTimeout(n, FindReverseNumberV2)
+	r3, err3 := execWithTimeout(n, FindReverseNumberV3)
 
-	// start = time.Now()
-	// fmt.Println(FindReverseNumberV3(100000)) // 10.25s -> slower than V2
-	// fmt.Printf("V3 took %s\n", time.Since(start))
+	fmt.Printf("V1 finding n=%d: <%d, %v>\n", n, r1, err1)
+	fmt.Printf("V2 finding n=%d: <%d, %v>\n", n, r2, err2)
+	fmt.Printf("V3 finding n=%d: <%d, %v>\n", n, r3, err3)
 
 	// Final version should do 100000000 in less than 12s
+}
+
+func execWithTimeout(n uint64, fn func(n uint64) uint64) (uint64, error) {
+	type result struct {
+		v   uint64
+		err error
+	}
+	res := make(chan result, 1)
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				res <- result{0, fmt.Errorf("panic: %v", r)}
+			}
+		}()
+		res <- result{v: fn(n)}
+	}()
+
+	select {
+	case r := <-res:
+		return r.v, r.err
+	case <-time.After(timeout):
+		return 0, fmt.Errorf("timeout after %fs", timeout.Seconds())
+	}
 }
 
 func FindReverseNumberV1(n uint64) uint64 {
